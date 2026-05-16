20634d5b8d2fac766ff86c4e3d60be24ecb48323
kek2774 <artem27742854@mail.ru>
2026-05-16 16:20:00 +0300

    interface changes

--- app_pages/page6_cube_spline.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Кубические сплайны", layout="wide")
-
-st.title(
-    r"Задание 6: выписать кубические сплайны дефекта 1 на каждом отрезке $ x \in [x_{i-1}, x_{i}], i=1,\dots,4$, $S^{''}(x_{1})=S^{''}(x_{6})$"
-)
-
-st.write("Хз что делать)")
